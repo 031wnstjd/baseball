a3498dcd1ec95f1053f8ed9e4b07d94593a371e2
hihi <you@example.com>
2025-06-04 13:44:33 +0900

    [refactoring] p34

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -17,17 +17,20 @@ class GameTest {
         assertNotNull(game);
     }
 
-    @Test
-    void throwExceptionWhenInputIsNull() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            game.guess(null);
-        });
-    }
 
     @Test
     void throwExceptionWhenInputLengthIsUnmatched() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            game.guess("12");
-        });
+        assertIllegalArgument(null);
+        assertIllegalArgument("12");
+        assertIllegalArgument("1234");
+    }
+
+    private void assertIllegalArgument(String guessNumber) {
+        try {
+            game.guess(guessNumber);
+            fail();
+        } catch (IllegalArgumentException e) {
+
+        }
     }
 }
